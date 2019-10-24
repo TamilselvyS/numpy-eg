02dacf4c07ffe5b3be6b9959981ee7dddd95dd3b
tamilselvysm23 <tamilselvysm23@gmail.com>
2019-10-24 23:25:23 +0530

    Numpy slicing, Iterating

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Numpy array (1D, 2D, 3D)
 Convert Image into Numpy Array
 Create Images out of an Numpy Array
+Indexing, Slicing and Iterating Numpy Array
--- imgToNumpy/main.py
+++ imgToNumpy/main.py
@@ -9,3 +9,28 @@ print(img_num2)
 # creating images out of an Numpy array
 cv2.imwrite("new-mario1.png", img_num1)
 cv2.imwrite("new-mario2.png", img_num2)
+
+x = cv2.imread("sample.png", 0)
+print(x)
+#Indexing and slicing
+print("\nIndexing and slicing Numpy array")
+print("\n 1st Two rows in Numpy array")
+print(x[0:2])
+print("\n 2nd and 3rd indexed element in 1st Two rows in Numpy array")
+print(x[0:2, 2:4])
+
+print(x[:, 2:4])
+print("\n Specific one element in numpy array")
+print(x[1,3])
+
+#Iterating Numpy Array
+print("\nIterating Numpy Array")
+print("\nIterating by Rows of Numpy array")
+for i in x:
+    print (i)
+print("\nIterating by Columns of Numpy array")
+for i in x.T:
+    print (i)
+print("\nIterating value by value")
+for i in x.flat:
+    print (i)
